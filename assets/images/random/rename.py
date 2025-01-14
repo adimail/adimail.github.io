4b27dbe463f573c518f01553b4e8ad97c7824fa8
Aditya <adimail2404@gmail.com>
2025-01-15 01:11:46 +0530

    new assets for /img and fun with flags update

--- assets/images/random/rename.py
+++ assets/images/random/rename.py
@@ -1,18 +1,59 @@
 import os
 
-
 directory_path = os.getcwd()
-jpg_files = [f for f in os.listdir(directory_path) if f.endswith(".jpeg")]
-jpg_files.sort()
-counter = 1
 
-for old_name in jpg_files:
-    extension = os.path.splitext(old_name)[1]
-    new_name = f"{counter}{extension}"
-    old_path = os.path.join(directory_path, old_name)
+jpeg_files = [f for f in os.listdir(directory_path) if f.endswith(".jpeg")]
+
+numbered_files = []
+non_numbered_files = []
+
+for file in jpeg_files:
+    name, _ = os.path.splitext(file)
+    if name.isdigit():
+        numbered_files.append(int(name))
+    else:
+        non_numbered_files.append(file)
+
+numbered_files.sort()
+
+max_number = numbered_files[-1] if numbered_files else 0
+all_numbers = set(range(1, max_number + 1))
+missing_numbers = sorted(all_numbers - set(numbered_files))
+
+for file in non_numbered_files:
+    if missing_numbers:
+        new_number = missing_numbers.pop(0)
+        new_name = f"{new_number}.jpeg"
+    else:
+        max_number += 1
+        new_name = f"{max_number}.jpeg"
+
+    old_path = os.path.join(directory_path, file)
     new_path = os.path.join(directory_path, new_name)
-
     os.rename(old_path, new_path)
-    counter += 1
 
-print("All .jpeg files have been renamed.")
+jpeg_files = [f for f in os.listdir(directory_path) if f.endswith(".jpeg")]
+numbered_files = []
+for file in jpeg_files:
+    name, _ = os.path.splitext(file)
+    if name.isdigit():
+        numbered_files.append(int(name))
+
+numbered_files.sort()
+
+max_number = numbered_files[-1] if numbered_files else 0
+all_numbers = set(range(1, max_number + 1))
+missing_numbers = sorted(all_numbers - set(numbered_files))
+
+remaining_files = [f for f in jpeg_files if not os.path.splitext(f)[0].isdigit()]
+remaining_files.sort()
+
+for file in reversed(sorted(jpeg_files)):
+    if missing_numbers:
+        old_path = os.path.join(directory_path, file)
+        new_number = missing_numbers.pop(0)
+        new_name = f"{new_number}.jpeg"
+        new_path = os.path.join(directory_path, new_name)
+        os.rename(old_path, new_path)
+
+print("All .jpeg files have been renamed and gaps filled.")
